1939fe70b7697bc918bb29cd1a45bd6c6560446f
harry <hkiang01@yahoo.com>
2015-12-01 18:24:05 -0600

    star wars is fun

--- linklayer.py
+++ linklayer.py
@@ -19,16 +19,36 @@ class Channel:
 		self.maxAttempt = M
 		self.simTime = t
 		self.channelOccupied = 0
+		self.utilCount = 0
+		self.idleCount = 0
 	def occupyChannel(self, nodeID):
 		self.channelOccupied = nodeID
 	def freeChennel(self):
 		self.channelOccupied = 0
+	def getOccupiedPct(self):
+		return self.occupiedCount/(self.occupiedCount + self.idleCount)
+	def getIdlePct(self):
+		return self.idleCount/(self.idleCount + self.occupiedCount)
 
 def parse(filename):
 	f = open(filename, "r")
 	lines = f.readlines()
 	for line in lines:
 		#grab initialization vars
+		curr = line.split(" ")
+		if(curr[0] == "N"):
+			print "numNodes: " + curr[1]
+		elif(curr[0] == "L"):
+			print "packetSize: " + curr[1]
+		elif(curr[0] == "R"):
+			for i in xrange(1, len(curr)-1, 1):
+				print "range: " + curr[i]
+		elif(curr[0] == "M"):
+			print "maxAttempts: " + curr[1]
+		elif(curr[0] == "T"):
+			print "simTime: " + curr[1]
+		else:
+			print "Error: Invalid input file"
 		print line
 
 def main():
