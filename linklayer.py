7a65292f508812ff3ec89a4954657b09b014a6ef
harry <hkiang01@yahoo.com>
2015-12-01 19:24:34 -0600

    see todo's

--- linklayer.py
+++ linklayer.py
@@ -4,16 +4,26 @@ import sys
 from random import randint
 
 class Node:
-	def __init__(self, ID=-1):
+	def __init__(self, ID=-1, maxAttempts=0):
 		self.ID = ID
+		self.maxAttempts = maxAttempts
 		self.backoff = -1
 		self.currRange = 0
+		self.tryCount = 0
+		self.isAwake = True
 	def setBackoff(self, R):
 		self.backoff = randint(0,R)
 	def countDown(self):
 		self.backoff = self.backoff-1;
 	def setRange(self, newRange):
 		self.currRange = newRange
+	def collision(self):
+		if(self.isAwake):
+			self.tryCount = self.tryCount + 1
+			if(self.tryCount >= self.maxAttempts):
+				self.isAwake = False
+			# todo: itereate range (need access to ranges array)
+			# todo: new random number based on new range (access same array)
 
 class Channel:
 	def __init__ (self, args):
@@ -21,14 +31,15 @@ class Channel:
 		self.numNodes = N
 		self.packetSize = L
 		self.ranges = R # a list
-		self.maxAttempt = M
+		self.maxAttempts = M
 		self.simTime = T
 		self.channelOccupied = 0
 		self.utilCount = 0
 		self.idleCount = 0
 		self.nodes = []
+		self.collisions = 0
 		for i in range(self.numNodes+1):
-			self.nodes.append(Node(i))
+			self.nodes.append(Node(i, self.maxAttempts))
 	def occupyChannel(self, nodeID):
 		self.channelOccupied = nodeID
 	def freeChennel(self):
@@ -42,6 +53,19 @@ class Channel:
 			node.setRange(self.ranges[0])
 			node.setBackoff(node.currRange)	
 			print node.backoff
+	def tick(self):
+		zerolist = []
+		for node in self.nodes:
+			if(node.backoff==0):
+				zerolist.append(node)
+			node.countDown()
+		if(len(zerolist) != 0):
+			# zerolist[0] will own the channel
+			channelOccupied = zerolist[0].ID
+			self.collisions += len(zerolist) - 1
+			for i in xrange(1, len(zerolist), 1):
+				zerolist[i].collision()
+
 def parse(filename):
 	f = open(filename, "r")
 	lines = f.readlines()
@@ -91,7 +115,8 @@ def main():
 	channel = Channel(args)
 
 	channel.initNodes()
-	
+	for simulation in xrange(0, channel.simTime):
+		channel.tick()
 
 
 if __name__ == '__main__':
